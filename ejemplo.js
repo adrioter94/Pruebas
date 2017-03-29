4c0e0a320af3411e7922efbdc907deeae492b132
adrioter94 <adrioter94@hotmail.es>
2017-03-29 13:23:57 +0200

    Update ejemplo.js

--- ejemplo.js
+++ ejemplo.js
@@ -1,25 +1,5 @@
-// Get some JSON text via Ajax, and write it to the p HTML page
-// Get new text when clicking on "third"
-//
-
-// function write_sentence () {
-    // var sentence = [];
-    //I cannot just iterate, with $.each, over words_global,
-    //because object is unordered
-    // $.each( ["start", "middle", "end"], function (key, val) {
-	// var list = words_global[val];
-	// console.log (list);
-	// selection = Math.floor(Math.random() * list.length);
-	// sentence.push(list[selection])
-	// console.log (sentence);
-    // });
-    // $('#sentence').html(sentence.join(' '));
-// };
-
 $(document).ready(function() {
 	$.getJSON( "ejemplo.json", function(json) {
-		console.log("Hola3");
-		console.log( "JSON Data: " + json.items.length);
 		var i = 0;
 		for(i = 0; i < json.items.length; i++){
 			var ul = document.getElementById("list");
