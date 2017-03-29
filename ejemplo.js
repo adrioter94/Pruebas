fb2fc7c917fbddf5efdbc923147b1977f99b9b7e
adrioter94 <adrioter94@hotmail.es>
2017-03-29 11:10:28 +0200

    Add files via upload

---
+++ ejemplo.js
@@ -0,0 +1,23 @@
+// Get some JSON text via Ajax, and write it to the p HTML page
+// Get new text when clicking on "third"
+//
+
+// function write_sentence () {
+    // var sentence = [];
+    //I cannot just iterate, with $.each, over words_global,
+    //because object is unordered
+    // $.each( ["start", "middle", "end"], function (key, val) {
+	// var list = words_global[val];
+	// console.log (list);
+	// selection = Math.floor(Math.random() * list.length);
+	// sentence.push(list[selection])
+	// console.log (sentence);
+    // });
+    // $('#sentence').html(sentence.join(' '));
+// };
+
+$(document).ready(function() {
+	$.getJSON( "ejemplo.json", function(json) {
+		console.log( "JSON Data: " + json.item);
+	});
+});
