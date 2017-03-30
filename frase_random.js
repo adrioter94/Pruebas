f37fe9441fa82c6fbf1f816c2702a2b04b427bf3
adrioter94 <adrioter94@hotmail.es>
2017-03-30 14:13:56 +0200

    Update frase_random.js

--- frase_random.js
+++ frase_random.js
@@ -1,36 +1,13 @@
-// function write_sentence () {
-    // var sentence = [];
-    //I cannot just iterate, with $.each, over words_global,
-    //because object is unordered
-    // $.each( ["start", "middle", "end"], function (key, val) {
-	// var list = words_global[val];
-	// console.log (list);
-	// selection = Math.floor(Math.random() * list.length);
-	// sentence.push(list[selection])
-	// console.log (sentence);
-    // });
-    // $('#sentence').html(sentence.join(' '));
-// };
-
-function generate_sentence(obj){
-	sentence = "";
-	$.each( obj, function( key, value ) {
-			selection = Math.floor(Math.random() * value.length);
-			sentence += value[selection] + " ";
-		});
-	$("sentence").html(sentence)
-}
 
 $(document).ready(function() {
 	var sentence = "";
-	var some = 2;
-	$.getJSON("frase_random.json", function(words){
-		console.log(some);
-		some = words;
-		console.log(some);
-	});
-	console.log(some);
 	$("#btn").click(function(){
-		generate_sentence(words);
+		$.getJSON("frase_random.json", function(words){
+			$.each( words, function( key, value ) {
+				selection = Math.floor(Math.random() * value.length);
+				sentence += value[selection] + " ";
+			});
+			$("sentence").html(sentence)
+		});
 	});
 });
