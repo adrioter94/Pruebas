7c43eb3e3d9a7f48279436b8cebc38c379e4bbcc
adrioter94 <adrioter94@hotmail.es>
2017-03-30 14:16:33 +0200

    Update frase_random.js

--- frase_random.js
+++ frase_random.js
@@ -2,11 +2,14 @@
 $(document).ready(function() {
 	var sentence = "";
 	$("#btn").click(function(){
+		sentence = "";
 		$.getJSON("frase_random.json", function(words){
 			$.each( words, function( key, value ) {
 				selection = Math.floor(Math.random() * value.length);
 				sentence += value[selection] + " ";
+				console.log("1.-" + sentence);
 			});
+			console.log("2.-" + sentence);
 			$("sentence").html(sentence)
 		});
 	});
