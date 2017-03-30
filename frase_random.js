abfeaf7a9233a7421ffe242b234562e19de34cc8
adrioter94 <adrioter94@hotmail.es>
2017-03-30 12:46:14 +0200

    Add files via upload

---
+++ frase_random.js
@@ -0,0 +1,22 @@
+// function write_sentence () {
+    // var sentence = [];
+    //I cannot just iterate, with $.each, over words_global,
+    //because object is unordered
+    // $.each( ["start", "middle", "end"], function (key, val) {
+	// var list = words_global[val];
+	// console.log (list);
+	// selection = Math.floor(Math.random() * list.length);
+	// sentence.push(list[selection])
+	// console.log (sentence);
+    // });
+    // $('#sentence').html(sentence.join(' '));
+// };
+
+$(document).ready(function() {
+    $.getJSON("frase_random.json", function(words) {
+		console.log(words);
+	// words_global = words;
+	// write_sentence();
+    });
+    //$("#sentence").click(write_sentence);
+});
