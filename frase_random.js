d1a7bb211e7b4849b4fb1187105ddc03e9e0d93f
adrioter94 <adrioter94@hotmail.es>
2017-03-30 14:22:30 +0200

    Update frase_random.js

--- frase_random.js
+++ frase_random.js
@@ -1,19 +1,14 @@
 
 $(document).ready(function() {
 	var sentence = "";
-	$("sentence").html("Holis");
 	$("#btn").click(function(){
 		sentence = "";
 		$.getJSON("frase_random.json", function(words){
 			$.each( words, function( key, value ) {
 				selection = Math.floor(Math.random() * value.length);
 				sentence += value[selection] + " ";
-				console.log("1.-" + sentence);
 			});
-			console.log("2.-" + sentence);
-			$("sentence").html(sentence);
+			$("#sentence").html(sentence);
 		});
-		console.log("3.-" + sentence);
 	});
-	console.log("4.-" + sentence);
 });
